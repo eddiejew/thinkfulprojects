8834e8413517ce57de04d429229688c679e96d69
Eddie Jew <edward.w.jew@gmail.com>
2015-09-02 20:52:34 -0400

    Unit 2 lesson 5 challenge

---
+++ Unit2/multivariate.py
@@ -0,0 +1,32 @@
+"""multivariate.py"""
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+
+# import loans data
+loansData = pd.read_csv("/Users/eddiejew/thinkful/LoanStats3c.csv").astype(str)
+
+# remove rows with null values
+loansData.dropna(inplace=True)
+
+# remove '%' from InterestRate column
+loansData['int_rate'].map(lambda x: round(float(x.rstrip('%')), 4))
+
+# add intercept in data
+loansData['StatsModels.Intercept'] = loansData['int_rate'].map(lambda x: float(1.0))
+
+# add home_ownership to data
+loansData['home_ownership'] = pd.Categorical(loansData.home_ownershihp).labels
+
+# fit model
+X = sm.add_constant(loansData['annual_inc'])
+est = sm.OLS(loansData['int_rate'], X).fit()
+
+est.summary()
+
+est = smf.OLS(loansData['int_rate'], X + loansData['home_ownership'].fit()
+
+est.summary()
