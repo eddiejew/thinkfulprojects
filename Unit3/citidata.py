8eccbb4f0d1cf48d0e855feb0c0452d9f07af51b
Eddie Jew <edward.w.jew@gmail.com>
2015-09-26 12:41:50 -0400

    updated citidata

--- Unit3/citidata.py
+++ Unit3/citidata.py
@@ -53,7 +53,7 @@ cur = con.cursor()
 # create a stationary reference table that will not change with time
 with con:
 	cur.execute('DROP TABLE IF EXISTS citibike_reference')
-	cur.execute('CREATE TABLE citibike_reference ((id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT );)
+	cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT );')
 
 sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
 
@@ -98,7 +98,50 @@ with con:
 # go to our table, find the row that corresponds to the current execution time (if not, create it) then change the value in the column for the 1 station we care about
 
 time.sleep(60)
+
+con.close()
+
 # running 60 times == for i in xrange: 60, 
 # iterations = 0, wall iterations <= 0 - 60
 # use timelibrary: time.sleep(60)
 # data might take only 1 second to get, time.sleep(59)
+
+import pandas as pd
+import sqlite3 as lite
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+df = pd.read_sql_query("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
+
+hour_change = collections.defaultdict(int)
+for col in df.columns:
+	station_vals = df[col].tolist()
+	station_id = col[1:]
+	station_change = 0
+	for k, v in enumerate(station_vals):
+		if k < len(station_vals) - 1:
+			station_change += abs(station_vals[k] - station_vals[k+1])
+	hour_change[int(station_id)] = station_change #convert the station id back to integer
+
+def keyithmaxval(d):
+	# create a list of the dictionary's keys and values
+	v = list(d.values())
+	k = list(d.keys())
+
+	# return the key with the max value
+	return k[v.index(max(v))]
+
+# assign the max key to max_station
+max_station = keywithmaxval(hour_change)
+
+# query sqlite for reference information
+cur.execute("SELECT id, stationname, latitude, longitude FROM citibike_reference WHERE id = ?", (max_station,))
+data = cur.fetchone()
+print "The most active station is station id %s at %s latitude: %s longitude: %s " % data
+print "With " + str(hour_change[379]) + " bicycles coming and going in the hour between " + datetime.datetime.fromtimestamp(int(df.index[0])).strftime('%Y-%m-%dT%H:%M:%S') + " and " + datetime.datetime.fromtimestamp(int(df.index[-1])).strftime('%Y-%m-%dT%H:%M:%S')
+
+import matplotlib.pyplot as plt
+
+plt.bar(hour_change.keys(), hour_change.values())
+plt.show()
