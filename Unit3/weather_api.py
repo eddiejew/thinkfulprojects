f659793347744e0ec7a041c736b7f67b9b331902
Eddie Jew <edward.w.jew@gmail.com>
2015-10-05 21:10:54 -0400

    update weather_api

--- Unit3/weather_api.py
+++ Unit3/weather_api.py
@@ -24,6 +24,7 @@ con = lite.connect('weather.db')
 cur = con.cursor()
 
 with con:
+	cur.execute('DROP TABLE IF EXISTS temp')
 	cur.execute('CREATE TABLE IF NOT EXISTS temp ( day_recorded INT, Atlanta REAL, Denver REAL, Miami REAL, Nashville REAL, Seattle REAL )')
 
 # day_of_query starts 30 days in the past (from now)
@@ -39,8 +40,14 @@ with con:
 for k, v in cities.iteritems():
 	day_of_query = now - datetime.timedelta(days=30)
 	while day_of_query < now:
-		r = requests.get(my_api_url + v + ',' + day_of_query.strftime('%Y-%m-%dT12:00:00'))
+		r = requests.get(my_api_url + '/' + v + ',' + day_of_query.strftime('%Y-%m-%dT12:00:00'))
 		with con:
 			cur.execute('UPDATE temp SET ' + k + ' = ' + str(r.json()['daily']['data'][0]['temperatureMax']) + ' WHERE day_recorded = ' + day_of_query.strftime('%s'))
 		day_of_query += datetime.timedelta(days=1)
 con.close()
+
+# r => requests an HTTP file
+# response will include a header (status code, encoding (type of format), date + time, cookies)
+# response also includes a payload (JSON, file, etc)
+# 404 error: the API will not give you a reason why, we have to figure out ourselves
+# API key may be incorrect => look up API documentation to see how API server works
