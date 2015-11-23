6c4a433bc43edd424c5ae9ed3fdcc4d0d4656b33
Eddie Jew <edward.w.jew@gmail.com>
2015-11-22 22:19:01 -0500

    revised code

--- Unit4/random_forest.py
+++ Unit4/random_forest.py
@@ -1,71 +1,85 @@
 """random_forest.py"""
 
 import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_score
 import sklearn.ensemble as ske
 import sklearn.metrics as skm
-import numpy as np
 import matplotlib.pyplot as plt
 
-# load the feature names (as a dataframe) and convert to a list
-feature_df = pd.read_csv("samsungdata.csv",header=0)
-feature_list = list(feature_df.values.flatten())
+# read feature names
+feature_names = open("./features_clean.txt").read().splitlines()
+col_names = [f.split(" ")[1] for f in feature_names]
 
-# load the x_train set and use feature_list to label its columns
-xtrain = pd.read_csv("x_train.csv",header=None,names=feature_list)
-# load the y_train set
-ytrain = pd.read_csv("y_train.csv",header=None,names=["activity"])
+# create data frames from combined data
+df_X = pd.read_csv("./X_all.txt", sep=" *", engine="python", header=None, names=col_names)
+df_y = pd.read_csv("./y_all.txt", header=None, names=["activity"])
+df_sub = pd.read_csv("./subject_all.txt", header=None, names=["subject"])
 
-# set up a 500 estimator random forest
-clf = ske.RandomForestClassifier(n_estimators=500, oob_score=True)
-# fit the random forest to the x_train and y_train data
-clf = clf.fit(xtrain,ytrain.activity)
-# get the oob_score
-# oob_score: score of the training dataset obtained using an out-of-bag estimate
-print clf.oob_score
-# we get 0.9919
+# choose training, testing, and validation data - Scikit-learn doesn't 
+# natively support pandas. So we'll need to convert training and testing
+# sets into matrices for feeding to classifier algorithms
+
+training_set = df_sub["subject"] >= 27
+test_set = df_sub["subject"] <= 6
+cv_set = (df_sub["subject"] >= 21) & (df_sub["subject"] < 27)
+
+X_train = df_X[training_set].as_matrix()
+y_train = df_y[training_set].as_matrix().squeeze()
+
+X_test = df_X[test_set].as_matrix()
+y_test = df_y[test_set].as_matrix().squeeze()
+
+X_cv = df_X[cv_set].as_matrix()
+y_cv = df_y[cv_set].as_matrix().squeeze()
+
+# set up a 500 estimator random random_forest
+clf = RandomForestClassifier(n_estimators=500, oob_score=True)
+clf.fit(X_train, y_train) # fit estimator to the X_train + y_train data
+print clf.oob_score_
+# oob_score is 0.992592592593
 
 # get the importance scores
 importances = np.array(clf.feature_importances_)
 # get the top 10 indices of features
 top10_indices = importances.argsort()[-10:][::-1]
 print top10_indices
-# we get 56, 40, 52, 58, 49, 558, 53, 42, 50, 41: higher importance scores indicate more important features
-# which features are these?
-top10_features = [feature_list[x] for x in top10_indices]
+# we get 56, 40, 59, 52, 58, 558, 41, 50, 51, 42
+# which features are those?
+top10_features = [feature_names[x] for x in top10_indices]
 print top10_features
-# tGravityAcc_energy_X, tGravityAcc_Mean_X, tGravityAcc_min_X, tGravityAcc_energy_Z,
-# tGravityAcc_max_X, angle_X_gravityMean, tGravityAcc_min_Y, tGravityAcc_Mean_Z,
-# tGravityAcc_max_Y, tGravityAcc_Mean_Y
-# what are these scores?
+# we get '50 tGravityAccmaxX', '57 tGravityAccenergyX', '41 tGravityAccmeanX', 
+# '53 tGravityAccminX', '559 angleX_gravityMean', '59 tGravityAccenergyZ', 
+# '52 tGravityAccmaxZ', '42 tGravityAccmeanY', '54 tGravityAccminY', 
+# '43 tGravityAccmeanZ'
+# what are their scores?
 top10_scores = [importances[x] for x in top10_indices]
 print top10_scores
-# 0.03275m 0.03063, 0.03034, 0.02846, 0.02786, 0.02531. 0.01754, 0.01653, 0.01589
+# [0.034478784847599558, 0.032912251649066554, 0.032443059888468948, 0.030203796386248014, 
+# 0.029520767611458224, 0.029366880635201313, 0.017558907834240297, 0.016996435883561273, 
+# 0.01480349368966152, 0.014500245838005512]
 
-# find accuracy scores:
-# first we need to load the training and validation sets
-xtest = pd.read_csv("x_test.csv",header=None,names=feature_list)
-xval = pd.read_csv("x_validate.csv",header=None,names=feature_list)
-ytest = pd.read_csv("y_test.csv",header=None,names=["activity"])
-yval = pd.read_csv("y_validate.csv",header=None,names=["activity"])
-# load the predictions (as arrays)
-test_pred = clf.preict(xtest)
-val_pred = clf.predict(xval)
+# find the accuracy scores
+# first get the predictions (as arrays)
+test_pred = clf.predict(X_test)
+val_pred = clf.predict(X_cv)
 # now we can get the scores
-print skm.accuracy_score(ytest, test_pred)
-# accuracy score for test set: 0.90923
-print skm.accuracy_score(yval, val_pred)
-# accuracy score for validation set: 0.81780
+print skm.accuracy_score(y_test, test_pred)
+# accuracy score for test set: 0.8056
+print skm.accuracy_score(y_cv, val_pred)
+# accuary score for validation set: 0.8165
 
 # find precision, recall, and F1 scores on the test set
-print skm.precision_score(ytest, test_pred)
-# precision score 0.81887
-print skm.recall_score(ytest, test_pred)
-# 0.80923
-print skm.f1_score(ytest, test_pred)
-# 0.80710
+print skm.precision_score(y_test, test_pred)
+# precision score: 0.8189
+print skm.recall_score(y_test, test_pred)
+# recall score: 0.8092
+print skm.f1_score(y_test, test_pred)
+# F1 score: 0.8071
 
 # plot confusion matrix
-cm = skm.confusion_matrix(ytest, test_pred)
+cm = skm.confusion_matrix(y_test, test_pred)
 plt.matshow(cm)
 plt.title('Confusion Matrix')
 plt.colorbar()
